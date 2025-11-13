c674fb39fcb11b463f0776d14052b4195d28a1df
Abhay Sharma <abhaysharma.mrt@gmail.com>
2025-11-13 10:47:22 +0530

    Finally setup nativewind

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-sm">Edit app/index.tsx to edit this screen hello.</Text>
+    </View>
+  );
+}
--- components/Container.js
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView } from 'react-native';
-
-export const Container = ({ children }) => {
-  return <SafeAreaView className={styles.container}>{children}</SafeAreaView>;
-};
-
-const styles = {
-  container: 'flex flex-1 m-6',
-};
